b327269134177098529109594f18211348ec975a
ouali <ouali.aghiles@gmail.com>
2017-11-19 21:45:16 +0100

    Finalisation

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,3 +1,3 @@
-import test from '../sass/test.sass'
+// import test from '../sass/test.sass'
 console.log('Work fine !!')
-console.log(test)
+// console.log(test)
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,4 @@
 import config from './config'
 import scss from '../sass/main.scss'
-import sass from '../sass/test.sass'
 console.log(config)
-console.log(sass)
 console.log(scss)
